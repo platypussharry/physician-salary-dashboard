216074a0e6c256addb53b323e67640a73cbb4766
Tyler Polk <polkt.tyler@gmail.com>
2025-03-29 10:33:01 -0500

    backing up before moving to ssr

--- src/SalaryDashboard.js
+++ src/SalaryDashboard.js
@@ -1478,7 +1478,7 @@ const SalaryDrDashboard = () => {
                 className="text-sm text-indigo-600 font-medium flex items-center ml-auto"
                 onClick={handleViewAllSalaries}
               >
-                View all {aggregatedStats.totalSubmissions} salaries
+                View all {aggregatedStats.totalSubmissions.toLocaleString()} salaries
                 <svg className="ml-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
                 </svg>
